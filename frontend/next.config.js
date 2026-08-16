8dfe3b3f7564ab7891597e396d2de7f4756dbf57
Akshay Panigrahi <akshaypanigrahi7@gmail.com>
2026-08-16 09:24:24 +0530

    fix: Fix next.config.js syntax

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,13 @@
-{
-  "rewrites": [
-    {
-      "source": "/api/:path*",
-      "destination": "https://sia-chatbot-backend.onrender.com/api/:path*"
-    }
-  ]
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://sia-chatbot-backend.onrender.com/api/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
